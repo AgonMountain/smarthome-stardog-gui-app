f61a6ef3a69165277b496370853ed8467bd117ef
Yury Kiselev <agonmountain@outlook.com>
2021-12-05 01:04:20 +0300

    Обновлен интерфейс Добавлены функции для работы с DB

--- gui.py
+++ gui.py
@@ -1,73 +1,83 @@
 import PySimpleGUI as sg
 
+device_types = ('Laptop', 'Smartphone', 'Teapot')
+room_types = ('Bathroom', 'Bedroom', 'Children\'s room', 'Dining room', 'Kitchen', 'Living room', 'Office', 'Outside')
+
+actual_humans = [['yury kiselev'], ['victor noskin']]
+actual_devices = [['laptop1'], ['laptop2']]
+actual_rooms = [['room1'], ['room2']]
+actual_doors = [['door1'], ['door2']]
+actual_windows = [['window1'], ['window2']]
 
 
 tab1_humans_layout = [
-                [sg.Text('Name', size=(15, 1)), sg.InputText()],
-                [sg.Button(button_text='Save'), sg.Button(button_text='Cancel'),
-                 sg.Button(button_text='Add'), sg.Button(button_text='Delete')]
+                [sg.Table(headings=['Actual'], values=actual_humans)],
+                [sg.Text('Name', size=(15, 1)),
+                 sg.InputText()],
+                [sg.Text('Is now in (room)', size=(15, 1)),
+                 sg.InputCombo(actual_rooms, size=(20, 1), default_value='????')],
+                [sg.Button(button_text='Save'),
+                 sg.Button(button_text='Cancel'),
+                 sg.Button(button_text='Add'),
+                 sg.Button(button_text='Delete')]
             ]
 tab2_devices_layout = [
+                [sg.Table(headings=['Actual'], values=actual_devices)],
                 [sg.Text('Device type', size=(15, 1)),
-                 sg.InputCombo(('Laptop',
-                                'Smartphone',
-                                'Teapot'),
-                               size=(20, 1), default_value='Smartphone'),
+                 sg.InputCombo(device_types, size=(20, 1), default_value=device_types[0]),
                  sg.Button(button_text='Add new type')],
-                [sg.Text('Device name', size=(15, 1)), sg.InputText()],
-                [sg.Button(button_text='Save'), sg.Button(button_text='Cancel'),
-                 sg.Button(button_text='Add'), sg.Button(button_text='Delete')]
+                [sg.Text('Device name', size=(15, 1)),
+                 sg.InputText()],
+                [sg.Text('Is now in (room)', size=(15, 1)),
+                 sg.InputCombo(actual_rooms, size=(20, 1), default_value='????')],
+                [sg.Button(button_text='Save'),
+                 sg.Button(button_text='Cancel')]
             ]
 tab3_rooms_layout = [
+                [sg.Table(headings=['Actual'], values=actual_rooms)],
                 [sg.Text('Room type', size=(15, 1)),
-                 sg.InputCombo(('Bathroom',
-                                'Bedroom',
-                                'Childrensroom',
-                                'Diningroom',
-                                'Kitchen',
-                                'Livingroom',
-                                'Office',
-                                'Outside'),
-                               size=(20, 1), default_value='Bedroom'),
+                 sg.InputCombo(room_types, size=(20, 1), default_value=room_types[0]),
                  sg.Button(button_text='Add new type')],
-                [sg.Text('Room name', size=(15, 1)), sg.InputText()],
-                [sg.Button(button_text='Save'), sg.Button(button_text='Cancel'),
-                 sg.Button(button_text='Add'), sg.Button(button_text='Delete')]
+                [sg.Text('Room name', size=(15, 1)),
+                 sg.InputText()],
+                [sg.Button(button_text='Save'),
+                 sg.Button(button_text='Cancel')]
         ]
 tab4_doors_layout = [
+                [sg.Table(headings=['Actual'], values=actual_doors)],
                 [sg.Text('Door between', size=(15, 1)),
-                 sg.InputCombo(('A'),
-                               size=(20, 1), default_value='A'),
+                 sg.InputCombo('????', size=(20, 1), default_value='????'),
                  sg.Text('and', size=(3, 1)),
-                 sg.InputCombo(('B'),
-                               size=(20, 1), default_value='B')
-                 ],
-                [sg.Text('Door name', size=(15, 1)), sg.InputText()],
+                 sg.InputCombo('????', size=(20, 1), default_value='????')],
+                [sg.Text('Door name', size=(15, 1)),
+                 sg.InputText()],
                 [sg.Text('Status', size=(15, 1)),
-                    sg.Radio('Is Open', "RADIO_DOOR", size=(10,1)),
-                    sg.Radio('Is Closed', "RADIO_DOOR", default=True)],
-                [sg.Button(button_text='Save'), sg.Button(button_text='Cancel'),
-                 sg.Button(button_text='Add'), sg.Button(button_text='Delete')]
+                 sg.Radio('Is Open', "RADIO_DOOR", size=(10, 1)),
+                 sg.Radio('Is Closed', "RADIO_DOOR", default=True)],
+                [sg.Button(button_text='Save'),
+                 sg.Button(button_text='Cancel')]
             ]
 tab5_windows_layout = [
-                [sg.Text('Window name', size=(15, 1)), sg.InputText()],
+                [sg.Table(headings=['Actual'], values=actual_windows)],
+                [sg.Text('Window is in', size=(15, 1)),
+                 sg.InputCombo('????', size=(20, 1), default_value='????')],
+                [sg.Text('Window name', size=(15, 1)),
+                 sg.InputText()],
                 [sg.Text('Status', size=(15, 1)),
-                    sg.Radio('Is Open', "RADIO_WINDOW", size=(10,1)),
-                    sg.Radio('Is Closed', "RADIO_WINDOW", default=True)],
-                [sg.Button(button_text='Save'), sg.Button(button_text='Cancel'),
-                 sg.Button(button_text='Add'), sg.Button(button_text='Delete')]
+                 sg.Radio('Is Open', "RADIO_WINDOW", size=(10,1)),
+                 sg.Radio('Is Closed', "RADIO_WINDOW", default=True)],
+                [sg.Button(button_text='Save'),
+                 sg.Button(button_text='Cancel')]
             ]
 
-layout = [ [sg.TabGroup([[
-                sg.Tab('Devices', tab2_devices_layout),
-                sg.Tab('Rooms', tab3_rooms_layout),
-                sg.Tab('Doors', tab4_doors_layout),
-                sg.Tab('Windows', tab5_windows_layout)
-                ]])],
-            [sg.TabGroup([[
-                sg.Tab('Humans', tab1_humans_layout)
-                ]])]
-        ]
+layout = [[sg.TabGroup([[sg.Tab('Devices', tab2_devices_layout),
+                            sg.Tab('Rooms', tab3_rooms_layout),
+                            sg.Tab('Doors', tab4_doors_layout),
+                            sg.Tab('Windows', tab5_windows_layout)]])],
+            [sg.InputCombo('????', size=(40, 1), default_value='????'),
+             sg.Button(button_text='Connect'),
+             sg.Button(button_text='Disconnect')],
+            [sg.TabGroup([[sg.Tab('Humans', tab1_humans_layout)]])]]
 
 
 
--- stardog_db_manager.py
+++ stardog_db_manager.py
@@ -1,36 +1,30 @@
+import stardog
+import re
 
 
-def create_database(admin, name):
-  '''Создать базу данных'''
-  admin.new_database(name)
+def value(source):
+  value = []
+  for i in range(len(source['results']['bindings'])):
+    value.append(re.search('[_\w\/\.]+#([_\w]+)', source['results']['bindings'][i]['x']['value']).group(1))
+  return value
 
 
-def drop_database(admin, name):
-  '''Удалить базу данных'''
-  db = admin.database(name)
-  db.drop()
+def query(query_predicate, query_object, db_name, conn_details):
+  with stardog.Connection(db_name, **conn_details) as conn:
+    result = conn.select('PREFIX agn: <http://www.semanticweb.org/agonm/ontologies/2021/10/stardog-smarthome#> '
+                          'SELECT ?x WHERE { ?x ' + query_predicate + ' agn:' + query_object + '}')
+  return result
 
 
-def add(admin, database_name):
-  '''Добавить запись в базу данных'''
-  None
+conn_details = {
+  'endpoint': 'http://localhost:5820',
+  'username': 'admin',
+  'password': 'admin'
+}
 
-def update(admin, database_name):
-  '''Обновить запись в базе данных'''
-  None
+with stardog.Admin(**conn_details) as admin:
+  db = admin.database('db9')
 
-def remove(admin, database_name):
-  '''Удалить запись из базы данных'''
-  None
-
+  print(value(query('rdf:type', 'Human', 'db9', conn_details)))
 
 
-  #db = admin.new_database('db')
-
-  # with stardog.Connection('db', **conn_details) as conn:
-  #   conn.begin()
-  #   conn.add(stardog.content.File('./test/data/example.ttl'))
-  #   conn.commit()
-  #   results = conn.select('select * { ?a ?p ?o }')
-
-  # db.drop()
