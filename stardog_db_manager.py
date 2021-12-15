64baaf5e53d8f87f1c6d797bcc52ad092786e22c
Yury Kiselev <agonmountain@outlook.com>
2021-12-15 18:03:14 +0300

    Обновлен менеджер работы с базой данных Функция query() изменена, теперь в нее передается запрос целиком, а не по отдельности. Добавлена возможность передать префикс для запроса

--- stardog_db_manager.py
+++ stardog_db_manager.py
@@ -1,30 +1,37 @@
 import stardog
 import re
 
-
-def value(source):
-  value = []
-  for i in range(len(source['results']['bindings'])):
-    value.append(re.search('[_\w\/\.]+#([_\w]+)', source['results']['bindings'][i]['x']['value']).group(1))
-  return value
-
-
-def query(query_predicate, query_object, db_name, conn_details):
-  with stardog.Connection(db_name, **conn_details) as conn:
-    result = conn.select('PREFIX agn: <http://www.semanticweb.org/agonm/ontologies/2021/10/stardog-smarthome#> '
-                          'SELECT ?x WHERE { ?x ' + query_predicate + ' agn:' + query_object + '}')
-  return result
-
-
 conn_details = {
   'endpoint': 'http://localhost:5820',
   'username': 'admin',
   'password': 'admin'
 }
+db_name = 'smarthome'
 
-with stardog.Admin(**conn_details) as admin:
-  db = admin.database('db9')
+def value(source):
+  value = []
+  for i in range(len(source['results']['bindings'])):
+    if re.search('(#)', source['results']['bindings'][i]['x']['value']) != None:
+      value.append(re.search('[_\w\/\.]+#([_\w]+)', source['results']['bindings'][i]['x']['value']).group(1))
+    else:
+      value.append(source['results']['bindings'][i]['x']['value'])
+  return value
 
-  print(value(query('rdf:type', 'Human', 'db9', conn_details)))
 
 
+def query(query, prefix='PREFIX agn: <http://www.semanticweb.org/agonm/ontologies/2021/10/stardog-smarthome#>'):
+  with stardog.Connection(db_name, **conn_details) as conn:
+    result = conn.select(prefix + ' ' + query)
+  return value(result)
+
+
+def insert():
+  return None
+
+
+def update():
+  return None
+
+
+def delete():
+  return None
